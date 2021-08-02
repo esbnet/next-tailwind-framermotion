0edb594c61fdf1b906766bc93ce8f9d6a61d4226
Edmilson Soares <esbnet@gmail.com>
2021-08-01 22:03:18 -0300

    next, tailwind e framemotion start project

---
+++ components/Header.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Header: React.FC = () => {
+  return (
+    <header className="flex justify-center md:justify-between py-2 md:py-4 itens-center">
+      <Image src="/img/logo.svg" alt="Logo" width={150} height={33} />
+
+      <nav className="hidden md:block space-x-8">
+        <Link href="/nesxtjs">
+          <a className="tracking-wide hover:text-gray-300">Next.js</a>
+        </Link>
+        <Link href="/tailwind">
+          <a className="tracking-wide hover:text-gray-300">Taillwind</a>
+        </Link>
+        <Link href="/framemotion">
+          <a className="tracking-wide hover:text-gray-300">Framer Motion</a>
+        </Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,9 @@
+import Layout from "../components/Layout";
+
 export default function Home() {
-  return <h1>Guilherme Rodz</h1>
+  return (
+    <Layout>
+      <h1 className="text-4x1 sm:text-6xl lg:text-7xl mt-10 leading-none font-extrabold tracking-light text-gray-900">Construa sites modernos com agilidade sem precisar sair do HTML.</h1>
+    </Layout>
+  );
 }
