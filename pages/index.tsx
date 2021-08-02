525d6c0607dc376b5bf2e48535192d2d4e652ae2
Edmilson Soares <esbnet@gmail.com>
2021-08-02 13:04:45 -0300

    Concluído

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,51 @@
-import Layout from "../components/Layout";
+import { motion } from "framer-motion";
+import Link from "next/link";
+
+interface CardsProps {
+  href: string;
+  imgSrc: string;
+  layoutId: string;
+}
+
+const Card: React.FC<CardsProps> = ({ href, imgSrc, layoutId }) => {
+  return (
+    <Link href={href}>
+      <div className="mt-10 md:mt-24 relative h-20 md:h-40 bg-gray-200 rounded-xl cursor-pointer hover:bg-gray-50 transition">
+        <motion.img
+          src={imgSrc}
+          alt=""
+          className="w-full h-full"
+          layoutId={layoutId}
+        />
+      </div>
+    </Link>
+  );
+};
 
 export default function Home() {
   return (
-    <Layout>
-      <h1 className="text-4x1 sm:text-6xl lg:text-7xl mt-10 leading-none font-extrabold tracking-light text-gray-900">Construa sites modernos com agilidade sem precisar sair do HTML.</h1>
-    </Layout>
+    <div>
+      <h1 className="text-4x1 sm:text-6xl lg:text-7xl mt-10 md:mt-24 leading-none font-extrabold tracking-light text-gray-900">
+        Construa sites modernos com agilidade sem precisar sair do HTML.
+      </h1>
+
+      <div className="grid w-full grid-cols-1 grid-rows-3 gap-4 md:grid-cols-3 md:grid-rows-1">
+        <Card
+          href={"/nextjs"}
+          imgSrc={"/img/nextjs.svg"}
+          layoutId={"nextjs-logo"}
+        />
+        <Card
+          href={"/tailwind"}
+          imgSrc={"/img/tailwind.svg"}
+          layoutId={"tailwind-logo"}
+        />
+        <Card
+          href={"/framermotion"}
+          imgSrc={"/img/framermotion.svg"}
+          layoutId={"framermotion-logo"}
+        />
+      </div>
+    </div>
   );
 }
